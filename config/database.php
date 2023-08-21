62eadd260ce238df001ec53d859f24bcd02b6d89
S-hre29ya07 <singh.shreya2907@gmail.com>
2023-08-21 21:27:55 +0530

    file added in config

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-
-            'host' =>   'db5009547747.hosting-data.io' ,
-            'port' =>   '3306' ,
-            'database' =>   'dbs8096156' ,
-            'username' =>  'dbu1245122' ,
-            'password' =>   'Nexus2022nova!' ,
+            'url' => '',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
